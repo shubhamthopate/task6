620cf6b40c3fe8315669b3260a35beefbdc48f9f
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-21 14:55:56 +0530

    sdffsdgf

--- modules/module-dev2/main.tf
+++ modules/module-dev2/main.tf
@@ -1,9 +1,15 @@
+data "azurerm_resource_group" "example" {
+  
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
+ 
+}
 
 resource "azurerm_application_insights" "example" {
   name                = var.insight2dev
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   application_type    = "web"
 }
 
