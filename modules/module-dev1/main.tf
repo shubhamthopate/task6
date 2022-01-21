244cd5f9a2b91a319d850f4f56aeeb153f2a9d66
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-21 17:03:19 +0530

    sdfsd

--- modules/module-dev1/main.tf
+++ modules/module-dev1/main.tf
@@ -13,8 +13,8 @@ resource "azurerm_log_analytics_workspace" "example" {
 
 resource "azurerm_application_insights" "example" {
   name                = var.insightnamedev
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   workspace_id        = azurerm_log_analytics_workspace.example.id
   application_type    = "web"
 }
