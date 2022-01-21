27d1c599d5cf3e403923bdd88bc130c06bc9462c
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-21 14:36:57 +0530

    sfsefd

--- modules/module-dev2/main.tf
+++ modules/module-dev2/main.tf
@@ -5,8 +5,8 @@ data "azurerm_resource_group" "example" {
 
 resource "azurerm_application_insights" "example" {
   name                = var.insight2dev
-  location            = var.locationdev
-  resource_group_name = var.rgnamedev
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   application_type    = "web"
 }
 
