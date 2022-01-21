92f971ce2afb1686ffd04637e8429987d6129561
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-21 11:58:13 +0530

    sfdf

--- modules/module-dev1/main.tf
+++ modules/module-dev1/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_log_analytics_workspace" "example" {
 }
 
 resource "azurerm_application_insights" "example" {
-  name                = "tf-test-appinsights"
+  name                = "tf-test-st-appinsights"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   workspace_id        = azurerm_log_analytics_workspace.example.id
